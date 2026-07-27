de5cefd42d0d8525d925de038e681b8b292536e7
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-27 13:35:01 +0300

    Update ModEntities.java

--- src/main/java/com/example/doommod/init/ModEntities.java
+++ src/main/java/com/example/doommod/init/ModEntities.java
@@ -2,11 +2,13 @@ package com.example.doommod.init;
 
 import com.example.doommod.DoomMod;
 import com.example.doommod.entity.EntityDoomMob;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.common.registry.EntityRegistry;
 
 public class ModEntities {
     public static void register() {
         EntityRegistry.registerModEntity(
+                new ResourceLocation(DoomMod.MODID, "doommob"),
                 EntityDoomMob.class,
                 "doommob",
                 1,                 // унікальний id ентіті в межах мода

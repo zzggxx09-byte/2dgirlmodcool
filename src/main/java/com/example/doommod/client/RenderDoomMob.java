a7b5639a62f7832ace0ac93d94ce0888d6d150f4
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-27 13:34:06 +0300

    Update RenderDoomMob.java

--- src/main/java/com/example/doommod/client/RenderDoomMob.java
+++ src/main/java/com/example/doommod/client/RenderDoomMob.java
@@ -8,7 +8,7 @@ import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
-import net.minecraft.util.MathHelper;
+import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;

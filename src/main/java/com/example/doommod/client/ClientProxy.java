6c13ee05aded8863371de854ab87a6a7832f36d2
zzggxx09-byte <zzggxx09@gmail.com>
2026-07-27 14:15:05 +0300

    Update ClientProxy.java

--- src/main/java/com/example/doommod/client/ClientProxy.java
+++ src/main/java/com/example/doommod/client/ClientProxy.java
@@ -3,10 +3,13 @@ package com.example.doommod.client;
 import com.example.doommod.CommonProxy;
 import com.example.doommod.entity.EntityDoomMob;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
+import net.minecraftforge.fml.common.FMLLog;
 
 public class ClientProxy extends CommonProxy {
     @Override
     public void registerRenderers() {
+        FMLLog.log.info("!!! DOOMMOD: registerRenderers() called !!!");
         RenderingRegistry.registerEntityRenderingHandler(EntityDoomMob.class, RenderDoomMob::new);
+        FMLLog.log.info("!!! DOOMMOD: renderer registered !!!");
     }
 }
